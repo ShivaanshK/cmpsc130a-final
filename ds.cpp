24bfd514bdc8af06716c89d9ce7f7949e712361e
Sammy <sumezawa@protonmail.com>
2021-05-08 16:22:46 -0700

    semi-working BST. begin rbify BST.

--- ds.cpp
+++ ds.cpp
@@ -25,33 +25,134 @@ void RBFG::yeet(Node* n) {
 // functions for rb-tree
 // exact-match
 Node* RBFG::find(std::string& name, Node* n) const {
-    return nullptr;
+    if (n) {
+        if (n->name == name) {
+            return n;
+        }
+        else {
+            Node* child = find(name, n->left);
+            if (!child) {
+                child = find(name, n->right);
+            }
+            return child;
+        }
+    }
+    else {
+        return nullptr;
+    }
 }
 
 // range-search
-std::vector<std::string> RBFG::findrange(std::string& name1, std::string& name2, Node* n) const {
-    std::vector<std::string> bruh;
-    return bruh;
+std::vector<std::string> RBFG::findrange(std::string& name1, std::string& name2) const {
+    std::vector<std::string> namelist;
+    namelist.push_back(name1);
+    Node* traverse = successor(name1);
+    while (traverse->name != name2) {
+        namelist.push_back(traverse->name);
+        traverse = successor(traverse->name);
+    }
+    namelist.push_back(name2);
+    return namelist;
 }
 
 // helper, private function
 Node* RBFG::successor(std::string& name) const {
-    return nullptr;
+    Node* traverse = find(name, root);
+    if (traverse) {
+        // if this node has a right subtree
+        if (traverse->right) {
+            traverse = traverse->right;
+            while (traverse->left != nullptr) {
+                traverse = traverse->left;
+            }
+        }
+        // if this node has no right subtree, 
+        // starting from root to that node, the last left node
+        // passed before arriving at the node is the successor
+        else {
+            Node* lastleft = root;
+            while (lastleft->name != name) {
+                if (lastleft->name < name) {
+                    lastleft = lastleft->right;
+                }
+                else {
+                    traverse = lastleft;
+                    lastleft = lastleft->left;
+                }
+            }
+        }
+        // if the last left node passed is this node
+        // this node has no successor
+        if (traverse->name == name) {
+            return nullptr;
+        }
+        else {
+            return traverse;
+        }
+    }
+    else {
+        // root was nullptr OR
+        // name is not in rb-tree
+        return nullptr;
+    }
 }
 
 // insert
-bool RBFG::insert(std::string& name) {
-    return true;
+bool RBFG::insert(std::string& name, i graph_index, i file_index) {
+    if (root) {
+        return insert(name, graph_index, file_index, root);
+    }
+    else {
+        root = new Node(name, graph_index, file_index);
+        return true;
+    }
 }
 
 // helper, private function
-bool RBFG::insert(std::string& m, Node* n) {
-    return false;
+bool RBFG::insert(std::string& name, i graph_index, i file_index, Node* n) {
+    if (n->name == name) {
+        return false;
+    }
+    else {
+        if (n->name < name) {
+            if (n->right) {
+                return insert(name, graph_index, file_index, n->right);
+            }
+            else {
+                n->right = new Node(name, graph_index, file_index);
+                n->right->parent = n;
+                return true;
+            }
+        }
+        // already known that n->name != name
+        // checks n->name > name
+        else { 
+            if (n->left) {
+                return insert(name, graph_index, file_index, n->left);
+            }
+            else {
+                n->left = new Node(name, graph_index, file_index);
+                n->left->parent = n;
+                return true;
+            }
+        }
+    }
 }
 
 // functions for rb-tree & f-graph
+// print name and occupation given range of names
+void RBFG::print_range(std::string& name1, std::string& name2) const {
+    std::vector<std::string> namelist = findrange(name1, name2);
+    for (auto& name : namelist) {
+        // stub. implement filedata
+        std::cout << name << std::endl;
+    }
+    return;
+}
+
 // print all information for all users
 void RBFG::print_all() const {
+    std::cout << "print everything stub" << std::endl;
     return;
 }
 
--- ds.h
+++ ds.h
@@ -3,14 +3,19 @@
 #ifndef DS_H
 #define DS_H
 
+#include <iostream>
 #include <string>
-#include <vector> // range based queue return value
+#include <vector>
+
+typedef uint32_t i;
 
 struct Node {
-    Node(const std::string& n = std::string{})
-        : name{n}, parent(nullptr), left(nullptr), right(nullptr) {}
+    Node(const std::string& n = std::string{}, i gi = 0, i fi = 0)
+        : name{n}, graph_index{gi}, file_index{fi}, parent(nullptr), left(nullptr), right(nullptr) {}
     
     std::string name;
+    i graph_index;
+    i file_index;
     Node* parent;
     Node* left;
     Node* right;
@@ -23,28 +28,19 @@ class RBFG {
        
         // functions for rb-tree
         Node* find(std::string& name, Node* n) const;
-        std::vector<std::string> findrange(std::string& name1, std::string& name2, Node* n) const;
-        bool insert(std::string& name);
-        
+        std::vector<std::string> findrange(std::string& name1, std::string& name2) const;
+        bool insert(std::string& name, i graph_index, i file_index);
         
         // functions for rb-tree & f-graph
+        void print_range(std::string& name1, std::string& name2) const;
         void print_all() const;
         
     private:
         Node* root;
 
         void yeet(Node* n); // recursive destructor helper
-        bool insert(std::string& m, Node* n); // recursive insert helper
+        bool insert(std::string& name, i graph_index, i file_index, Node* n); // recursive insert helper
         Node* successor(std::string& name) const; // findrange helper
 };
 
-/*
-class FG {
-
-};
-
-class PD {
-
-};
-*/
 #endif
